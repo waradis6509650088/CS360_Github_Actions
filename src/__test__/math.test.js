6cb712fe593f7d61c140e9d8d4c1b00e500b7246
ajnooncstu <141294711+ajnooncstu@users.noreply.github.com>
2024-10-16 12:18:23 +0700

    Create math.test.js

---
+++ src/__test__/math.test.js
@@ -0,0 +1,15 @@
+const { add, subtract, multiply} = require('../math.js');
+
+describe('Simple Arithmetic', () => {
+  test('addition', () => {
+    expect(add(5, 3)).toBe(8);
+  });
+
+  test('subtract', () => {
+    expect(subtract(5, 1)).toBe(4);
+  });
+
+  test('multiply', () => {
+    expect(multiply(2, 3)).toBe(6);
+  });
+});
